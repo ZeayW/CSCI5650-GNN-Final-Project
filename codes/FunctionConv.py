0910dbeb721ba64f2c2b619776ad3311384d7877
ZeayW <16307110042@fudan.edu.cn>
2021-11-24 21:21:15 +0800

    Update FunctionConv.py

--- codes/FunctionConv.py
+++ codes/FunctionConv.py
@@ -69,6 +69,7 @@ class FunctionConv(nn.Module):
         # for each gate type, use an independent aggregator (function) to aggregate the messages
         for i in range(self.ntypes):
             mask = gate_types==i    # nodes of type i
+            print(gate_types.shape)
             print(self.gate_functions[i].weight.shape, gate_inputs.shape)
             res[mask] = self.gate_functions[i](gate_inputs[mask])
 
