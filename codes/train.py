ea4ce2bb72c4a1b6a5fb0e394e0830bdcc1d3d7d
ZeayW <16307110042@fudan.edu.cn>
2021-12-03 15:01:40 +0800

    Update train.py

--- codes/train.py
+++ codes/train.py
@@ -393,7 +393,7 @@ def train(options):
     with open(train_data_file,'rb') as f:
         train_g = pickle.load(f)
         train_graphs = dgl.unbatch(train_g)
-        train_graphs = train_graphs[:options.train_percent]
+        train_graphs = train_graphs[:int(options.train_percent)]
         train_g = dgl.batch(train_graphs)
     with open(val_data_file,'rb') as f:
         val_g = pickle.load(f)
